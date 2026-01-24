fd0386a583187a5710e7522a25a7b1366f292fbf
bryanjoao <bryan_joao109@hotmail.com>
2026-01-24 04:59:52 -0500

    Update data_preprocessing.py

--- Src/data_preprocessing.py
+++ Src/data_preprocessing.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 if __name__ == '__main__':
     # Split the training set for each wind turbine
-    data = pd.read_pickle('./data/train/MANF_2019.pkl')
+    data = pd.read_pickle('./data/train/WT_2019.pkl')
 
-    wt_names_list = ['MF02', 'MF03', 'MF04', 'MF05', 'MF06', 'MF07', 'MF09', 'MF12', 'MF13']
+    wt_names_list = ['WT2', 'WT3', 'WT4', 'WT5', 'WT6', 'WT7', 'WT9', 'MF12', 'MF13']
 
     for wt_name in wt_names_list:
         wt_dataframe = data.filter(like=wt_name)
@@ -12,9 +12,9 @@ if __name__ == '__main__':
 
 
     # Split the test set for each wind turbine
-    data = pd.read_pickle('./data/test/MANF_2022.pkl')
+    data = pd.read_pickle('./data/test/WT_2022.pkl')
 
-    wt_names_list = ['MF02', 'MF03', 'MF04', 'MF05', 'MF06', 'MF07', 'MF09', 'MF12', 'MF13']
+    wt_names_list = ['WT2', 'WT3', 'WT4', 'WT5', 'WT6', 'WT7', 'WT9', 'MF12', 'MF13']
 
     for wt_name in wt_names_list:
         wt_dataframe = data.filter(like=wt_name)
